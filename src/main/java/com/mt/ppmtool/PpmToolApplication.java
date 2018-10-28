8060e5b3f47ff472eb37a01613893bb604b2104a
mtaghipour <mtaghipour@mtaghipour.com>
2018-10-28 20:19:28 +0330

    First Commit

---
+++ src/main/java/com/mt/ppmtool/PpmToolApplication.java
@@ -0,0 +1,12 @@
+package com.mt.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmToolApplication.class, args);
+    }
+}
